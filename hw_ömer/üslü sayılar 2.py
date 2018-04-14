c8e7914607c950cf335de77419de6409f6d7ac59
omerkenan <36736125+omerkenan@users.noreply.github.com>
2018-04-14 18:24:50 +0300

    Ödevler bitti

---
+++ hw_ömer/üslü sayılar 2.py
@@ -0,0 +1,27 @@
+while 1:
+    calisma = 2
+    while calisma == 2:
+        veri = input("Üssünü almak istediğiniz tam sayıyı giriniz:")
+        try:
+            sayi = int(veri)
+            calisma = calisma - 1
+        except ValueError:
+            print("lütfen bir tam sayı giriniz!!!")
+        veri2 = input("Kaçıncı kuvvetini almak istediğinizi pozitif tam sayı cinsinden giriniz:")
+        try:
+            sayi2 = int(veri2)
+            calisma = calisma - 1
+        except ValueError:
+            print("lütfen pozitif bir tam sayı giriniz!!!")
+        silah = 1
+        def fonk(at,avrat,silah):
+            if avrat == 0:
+                return silah
+            elif avrat<0:
+                return "Lütfen pozitif bir tam sayı giriniz!!!"
+            else:
+                silah=silah*at
+                avrat=avrat-1
+                return fonk(at,avrat,silah)
+
+        print("Sonuc:",fonk(sayi,sayi2,silah))
