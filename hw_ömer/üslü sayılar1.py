03bcf4448dbf8d1a93eb5b9ac8cd907efd93acf3
omerkenan <36736125+omerkenan@users.noreply.github.com>
2018-04-15 21:19:54 +0300

    Instroduction update

--- hw_ömer/üslü sayılar1.py
+++ hw_ömer/üslü sayılar1.py
@@ -2,12 +2,12 @@ while 1:
     calisma = 1
     while calisma ==1:
         print("Bu program girdiğiniz sayıyı 2 nin kuvveti şeklinde alır.\n")
-        veri = input("Lütfen bir tam sayı giriniz:")
+        veri = input("Lütfen 0 ya da pozitif bir tam sayı giriniz:")
         try:
             sayi = int(veri)
             calisma = 0
         except ValueError:
-            print("lütfen bir tam sayı giriniz!!!")
+            print("lütfen 0 ya da pozitif bir tam sayı giriniz!!!")
         helper = 2
         def işlem(at,helper):
             if(at == 0):
