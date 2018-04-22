b28597c4f367b30654ce5e38441c112a7c4dcc0d
Furkan Ardıç <furkan.ardic.98@gmail.com>
2018-04-22 21:47:41 +0300

    Turkish latters and hw_2 are fixed.

--- hw2/furkan/HW_2.py
+++ hw2/furkan/HW_2.py
@@ -1,10 +1,10 @@
-import tkinter as tk
-
-pencere = tk.Tk()
-pencere.geometry('1280x768')
-
-etiket = tk.Label(text='Merhaba Zalim Dünya')
-etiket.pack()
-düğme = tk.Button(text='Tamam', command=pencere.destroy)
-düğme.pack()
-pencere.mainloop()
+import requests
+market=input("Please give market name(btc-etc-usdt):")
+money=input("Please give money unit:")
+try:
+    url = "https://bittrex.com/api/v1.1/public/getmarketsummary?market={}-{}".format(market,money)
+    response = requests.get(url)
+    data = response.json()
+    print("1 {} is {} {}".format(money ,data["result"][0]["Last"],market ))
+except:
+    print("Please give useble market and money name.")
--- hw3/furkan/HW_3.py
+++ hw3/furkan/HW_3.py
@@ -5,18 +5,17 @@ response = requests.get(url)
 data = response.json()
 def btc_markets():
     #market listesi oluşturuluyor.
-    liste_btc_markets = []
+    list_btc_markets = []
     button_distance_y = 0.0
     button_distance_x = 0.0
     sliding_variable=1
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "B":
-            liste_btc_markets += [data["result"][i]["MarketName"]]
-    ilk_market="btc"
+            list_btc_markets += [data["result"][i]["MarketName"]]
     #liste butonlara atanıyor.
-    for i in range(len(liste_btc_markets)):
+    for i in range(len(list_btc_markets)):
         button_distance_y +=0.05
-        button_btc_market_i = tk.Button(text=liste_btc_markets[i][4:len(liste_btc_markets[i])])
+        button_btc_market_i = tk.Button(text=list_btc_markets[i][4:len(list_btc_markets[i])])
         button_btc_market_i.config(width=5)
         button_btc_market_i.config(font=("Courier", 10))
         button_btc_market_i.place(relx=0.25+button_distance_x , rely=0.0+button_distance_y)
@@ -25,17 +24,16 @@ def btc_markets():
             button_distance_y =0.1
             sliding_variable +=1
 def eth_markets():
-    liste_eth_markets = []
+    list_eth_markets = []
     button_distance_y = 0.0
     button_distance_x = 0.0
     sliding_variable = 1
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "E":
-            liste_eth_markets += [data["result"][i]["MarketName"]]
-    ilk_market = "eth"
-    for i in range(len(liste_eth_markets)):
+            list_eth_markets += [data["result"][i]["MarketName"]]
+    for i in range(len(list_eth_markets)):
         button_distance_y += 0.05
-        button_eth_market_i = tk.Button(text=liste_eth_markets[i][4:len(liste_eth_markets[i])])
+        button_eth_market_i = tk.Button(text=list_eth_markets[i][4:len(list_eth_markets[i])])
         button_eth_market_i.config(width=5)
         button_eth_market_i.config(font=("Courier", 10))
         button_eth_market_i.place(relx=0.25 + button_distance_x, rely=0.0 + button_distance_y)
@@ -44,17 +42,16 @@ def eth_markets():
             button_distance_y =0.1
             sliding_variable +=1
 def usdt_markets():
-    liste_usdt_markets = []
+    list_usdt_markets = []
     button_distance_y = 0.0
     button_distance_x = 0.0
     sliding_variable = 1
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "U":
-            liste_usdt_markets += [data["result"][i]["MarketName"]]
-    ilk_market = "usdt"
-    for i in range(len(liste_usdt_markets)):
+            list_usdt_markets += [data["result"][i]["MarketName"]]
+    for i in range(len(list_usdt_markets)):
         button_distance_y += 0.05
-        button_usdt_market_i = tk.Button(text=liste_usdt_markets[i][5:len(liste_usdt_markets[i])])
+        button_usdt_market_i = tk.Button(text=list_usdt_markets[i][5:len(list_usdt_markets[i])])
         button_usdt_market_i.config(width=5)
         button_usdt_market_i.config(font=("Courier", 10))
         button_usdt_market_i.place(relx=0.25 + button_distance_x, rely=0.0 + button_distance_y)
@@ -62,8 +59,8 @@ def usdt_markets():
             button_distance_x = 0.07 * sliding_variable
             button_distance_y = 0.1
             sliding_variable += 1
-pencere = tk.Tk()
-pencere.geometry('1280x768+-9+0')
+screen = tk.Tk()
+screen.geometry('1280x768+-9+0')
 
 headline= tk.Label(text='MARKETS')
 headline.config(width=200)
@@ -85,8 +82,8 @@ button_market_3.config(width=10)
 button_market_3.config(font=("Courier",30))
 button_market_3.place(relx=0.015, rely=0.342)
 
-button_exit = tk.Button(text='EXIT', command=pencere.destroy)
+button_exit = tk.Button(text='EXIT', command=screen.destroy)
 button_exit.config(width=5)
 button_exit.config(font=("Courier",20))
 button_exit.place(relx=0.92,rely=0.92)
-pencere.mainloop()
+screen.mainloop()
