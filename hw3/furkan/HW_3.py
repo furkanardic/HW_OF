79ca240a6e1941520bc34ad14cb534b0d4718820
Furkan Ardıç <furkan.ardic.98@gmail.com>
2018-04-24 23:05:33 +0300

    List boxes fix in screen but cod havent got any command.

--- hw3/furkan/HW_3.py
+++ hw3/furkan/HW_3.py
@@ -5,7 +5,6 @@ url = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
 response = requests.get(url)
 data = response.json()
 def btc_markets():
-    top = Tk()
     # market listesi oluşturuluyor.
     list_btc_markets=[]
     for i in range(len(data["result"])):
@@ -13,33 +12,27 @@ def btc_markets():
             list_btc_markets += [data["result"][i]["MarketName"]]
     #liste butonlara atanıyor.
     for i in range(len(list_btc_markets)):
-        Lb1 = Listbox(top)
+        Lb1 = Listbox(screen)
         Lb1.insert(i,list_btc_markets[i][4:len(list_btc_markets[i])])
-        Lb1.pack()
-    top.mainloop()
+        Lb1.pack(side = RIGHT)
 def eth_markets():
-
-    top = Tk()
     list_eth_markets = []
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "E":
             list_eth_markets += [data["result"][i]["MarketName"]]
     for i in range(len(list_eth_markets)):
-        Lb2 =Listbox(top)
+        Lb2 =Listbox(screen)
         Lb2.insert(i,list_eth_markets[i][4:len(list_eth_markets[i])])
-        Lb2.pack()
-    top.mainloop()
+        Lb2.pack(side=RIGHT)
 def usdt_markets():
-    top = Tk()
     list_usdt_markets = []
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "U":
             list_usdt_markets += [data["result"][i]["MarketName"]]
     for i in range(len(list_usdt_markets)):
-        Lb2 =Listbox(top)
-        Lb2.insert(i,list_usdt_markets[i][5:len(list_usdt_markets[i])])
-        Lb2.pack()
-    top.mainloop()
+        Lb3 =Listbox(screen)
+        Lb3.insert(i,list_usdt_markets[i][5:len(list_usdt_markets[i])])
+        Lb3.pack(side=RIGHT)
 screen = tk.Tk()
 screen.geometry('1280x768+-9+0')
 
@@ -48,25 +41,18 @@ headline.config(width=200)
 headline.config(font=("Courier", 44))
 headline.pack()
 
-
-button_market_1 = tk.Button(text='BTC', command=btc_markets)
-button_market_1.config(width=10)
-button_market_1.config(font=("Courier",30))
-button_market_1.place(relx=0.015,rely=0.150)
-
-button_market_2 = tk.Button(text='ETH', command=eth_markets)
-button_market_2.config(width=10)
-button_market_2.config(font=("Courier",30))
-button_market_2.place(relx=0.015, rely=0.246)
-
-button_market_3 = tk.Button(text='USDT', command=usdt_markets)
-button_market_3.config(width=10)
-button_market_3.config(font=("Courier",30))
-button_market_3.place(relx=0.015, rely=0.342)
-
 button_exit = tk.Button(text='EXIT', command=screen.destroy)
 button_exit.config(width=5)
 button_exit.config(font=("Courier",20))
 button_exit.place(relx=0.92,rely=0.92)
+
+Lb4 = Listbox(screen)
+Lb4.insert(1,"BTC")
+Lb4.insert(2,"ETH")
+Lb4.insert(3,"USDT")
+Lb4.config(width=7)#en
+Lb4.config(font=("Courier",25))
+Lb4.pack(side=LEFT , fill=BOTH)
+
 screen.mainloop()
 
