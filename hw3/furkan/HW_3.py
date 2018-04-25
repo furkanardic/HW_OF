a6f7cfdb4e58d1b88d93f190c84267b0d2ee6d32
Furkan Ardıç <furkan.ardic.98@gmail.com>
2018-04-26 01:49:01 +0300

    lists are fixed and graph datas are available but in screen.

--- hw3/furkan/HW_3.py
+++ hw3/furkan/HW_3.py
@@ -4,42 +4,92 @@ import requests
 url = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
 response = requests.get(url)
 data = response.json()
+btc=0
+eth=0
+usdt=0
+market=""
+money=""
+def scale():#grafik değerlerini x ve y koordinatlarını ayrı ayrı olarak listeye atıyor.
+    global market
+    global money
+    market = Lb4.get("active")
+    money = Lb1.get("active")
+    prices = []
+    times = []
+    url = "https://bittrex.com/api/v1.1/public/getmarkethistory?market={}-{}".format(market, money)
+    response = requests.get(url)
+    data = response.json()
+    for i in range(len(data["result"])):
+        prices += [data["result"][i]["Price"]]
+        times += [data["result"][i]["TimeStamp"]]
+    for i in range(len(prices)):
+        print(prices[i])
+    for i in range(len(times)):
+        print(times[i][0:19])
+def markets():#2.listeleri bastırıyor.
+    if (Lb4.get("active")=="BTC")and(btc==0)and(eth==0)and(usdt==0):
+        btc_markets()
+    elif (Lb4.get("active")=="ETH")and(btc==0)and(eth==0)and(usdt==0):
+        eth_markets()
+    elif (Lb4.get("active")=="USDT")and(btc==0)and(eth==0)and(usdt==0):
+        usdt_markets()
 def btc_markets():
+    global btc
+    btc=1
     # market listesi oluşturuluyor.
     list_btc_markets=[]
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "B":
             list_btc_markets += [data["result"][i]["MarketName"]]
-    #liste butonlara atanıyor.
+    #liste listboxa kopyalanıyor.
+
     for i in range(len(list_btc_markets)):
-        Lb1 = Listbox(screen)
         Lb1.insert(i,list_btc_markets[i][4:len(list_btc_markets[i])])
-        Lb1.pack(side = RIGHT)
+    Lb1.config(width=8)#en'i ayarlanıyor.
+    Lb1.config(font=("Courier", 20))
+    Lb1.pack(side=LEFT , fill=BOTH)#sola yapıştırıp , tamamını dolduruyor.
 def eth_markets():
+    global eth
+    eth=1
     list_eth_markets = []
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "E":
             list_eth_markets += [data["result"][i]["MarketName"]]
     for i in range(len(list_eth_markets)):
-        Lb2 =Listbox(screen)
-        Lb2.insert(i,list_eth_markets[i][4:len(list_eth_markets[i])])
-        Lb2.pack(side=RIGHT)
+        Lb1.insert(i,list_eth_markets[i][4:len(list_eth_markets[i])])
+    Lb1.config(width=8)
+    Lb1.config(font=("Courier",20))
+    Lb1.pack(side=LEFT , fill=BOTH)
 def usdt_markets():
+    global usdt
+    usdt=1
     list_usdt_markets = []
     for i in range(len(data["result"])):
         if data["result"][i]["MarketName"][0] == "U":
             list_usdt_markets += [data["result"][i]["MarketName"]]
     for i in range(len(list_usdt_markets)):
-        Lb3 =Listbox(screen)
-        Lb3.insert(i,list_usdt_markets[i][5:len(list_usdt_markets[i])])
-        Lb3.pack(side=RIGHT)
+        Lb1.insert(i,list_usdt_markets[i][5:len(list_usdt_markets[i])])
+    Lb1.config(width=8)
+    Lb1.config(font=("Courier",20))
+    Lb1.pack(side=LEFT , fill=BOTH)
 screen = tk.Tk()
 screen.geometry('1280x768+-9+0')
+Lb1 = Listbox(screen)
 
 headline= tk.Label(text='MARKETS')
 headline.config(width=200)
 headline.config(font=("Courier", 44))
 headline.pack()
+
+button_search = tk.Button(screen, text="SEARCH", command=markets)
+button_search.config(width=7)
+button_search.config(font=("Courier",20))
+button_search.place(relx=0.01,rely=0.01)
+
+button_search_currency = tk.Button(screen, text="SEARCH CURRENCY", command=scale)
+button_search_currency.config(width=20)
+button_search_currency.config(font=("Courier",20))
+button_search_currency.place(relx=0.12,rely=0.01)
 
 button_exit = tk.Button(text='EXIT', command=screen.destroy)
 button_exit.config(width=5)
@@ -55,4 +105,3 @@ Lb4.config(font=("Courier",25))
 Lb4.pack(side=LEFT , fill=BOTH)
 
 screen.mainloop()
-
