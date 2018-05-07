10bc8df9e775fb5cded88108c5b0b12db3f58a7b
Omer Kenan Ucmaklioglu <omerkenanuc@gmail.com>
2018-05-07 13:40:58 +0300

    Gui changed and thread is not working

--- hw3/omer/statistical-data-flow.py
+++ hw3/omer/statistical-data-flow.py
@@ -8,10 +8,38 @@ import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 
-
+exitFlag = 0
 unit_1="BTC"
 unit_2="ETH"
 unit_3="USDT"
+
+class refresh(threading.Thread):
+    def __init__(self, value1, value2):
+        threading.Thread.__init__(self)
+        self.value1 = value1
+        self.value2 = value2
+    def run(self):
+        while 1:
+            url = "https://bittrex.com/api/v1.1/public/getmarkethistory?market={}-{}".format(self.value1, self.value2)
+            variable = requests.get(url)
+            data = variable.json()
+            time.sleep(1)
+
+class process(threading.Thread):
+    def __init__(self,data):
+        threading.Thread.__init__(self)
+        self.data = data
+    def run(self):
+
+        if self.data["success"] == False:
+            print("Source is not accessible!!!")
+        else:
+            list1 = []
+            list2 = []
+            for i in range(len(self.data["result"])):
+                list1 = list1 + [self.data["result"][i]["TimeStamp"][11:19]]
+                list2 = list2 + [self.data['result'][i]["Price"]]
+
 
 class Window(QtWidgets.QWidget):
 
@@ -20,26 +48,35 @@ class Window(QtWidgets.QWidget):
         self.user_gui()
 
     def user_gui(self):
-        self.l =QtWidgets.QLabel("Please pick your unit...")
+        self.l =QtWidgets.QLabel("   E-Money Indexor...\nPlease pick your unit...")
         self.b = QtWidgets.QPushButton("Enter")
 
         self.listWidget = QtWidgets.QListWidget()
         self.listWidget.addItem(unit_1)
         self.listWidget.addItem(unit_2)
         self.listWidget.addItem(unit_3)
+        self.listWidget.setFixedSize(75,100)
 
         self.listWidget2 = QtWidgets.QListWidget()
+        self.listWidget2.setFixedSize(75, 100)
 
         h_box = QtWidgets.QHBoxLayout()
         h_box.addStretch()
         h_box.addWidget(self.l)
         h_box.addStretch()
 
+        h_box2 = QtWidgets.QHBoxLayout()
+        h_box2.addStretch()
+        h_box2.addWidget(self.listWidget)
+        h_box2.addWidget(self.listWidget2)
+        h_box2.addStretch()
+
         v_box = QtWidgets.QVBoxLayout()
+        v_box.addStretch()
         v_box.addLayout(h_box)
         v_box.addWidget(self.b)
-        v_box.addWidget(self.listWidget)
-        v_box.addWidget(self.listWidget2)
+        v_box.addLayout(h_box2)
+        v_box.addStretch()
 
         self.setLayout(v_box)
         self.setWindowTitle("E-Money indexor...")
@@ -53,27 +90,22 @@ class Window(QtWidgets.QWidget):
         sender = self.sender()
         if sender.text() == "Enter":
             try:
-                url = "https://bittrex.com/api/v1.1/public/getmarkethistory?market={}-{}".format(self.listWidget.currentItem().text(),self.listWidget2.currentItem().text())
-                variable = requests.get(url)
-                data = variable.json()
-                if data["success"] == False:
-                    print("Source is not accessible!!!")
-                else:
-                    list1 = []
-                    list2 = []
-                    for i in range(len(data["result"])):
-                        list1 = list1 + [data["result"][i]["TimeStamp"][11:19]]
-                        list2 = list2 + [data['result'][i]["Price"]]
-                    plt.plot(list1,list2)
-                    plt.ylabel("Price")
-                    plt.xlabel("Time - Almost 20 Minutes")
-                    plt.grid(True)
-                    plt.yscale('linear')
-                    plt.xscale('linear')
-                    plt.show()
+                thread = refresh(self.listWidget.currentItem().text(), self.listWidget2.currentItem().text())
+                thread2 = process(self.data)
+                thread.start()
+                thread.join()
+                thread2.start()
+                thread2.join()
+                plt.plot(self.list1, self.list2)
+                plt.ylabel("Price")
+                plt.xlabel("Time - Almost 20 Minutes")
+                plt.grid(True)
+                plt.yscale('linear')
+                plt.xscale('linear')
+                plt.show()
 
             except:
-                print("Please try to contact your developer...")
+                self.l.setText("Please try to contact your developer...")
 
 
     def baron(self):
@@ -103,3 +135,7 @@ class Window(QtWidgets.QWidget):
 app = QtWidgets.QApplication(sys.argv)
 a_window = Window()
 sys.exit(app.exec_())
+
+
+#self.listWidget.currentItem().text()
+#self.listWidget2.currentItem().text()
