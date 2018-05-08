18cb9e962d6a597bfcc84fe9f9bd9340ba62f6e3
Omer Kenan Ucmaklioglu <omerkenanuc@gmail.com>
2018-05-08 16:00:09 +0300

    scroll wheel added but not tested

--- hw3/omer/statistical-data-flow.py
+++ hw3/omer/statistical-data-flow.py
@@ -41,6 +41,40 @@ class process(threading.Thread):
                 list2 = list2 + [self.data['result'][i]["Price"]]
 
 
+def zoom_factory(ax,base_scale = 2.):
+    def zoom_fun(event):
+        # get the current x and y limits
+        cur_xlim = ax.get_xlim()
+        cur_ylim = ax.get_ylim()
+        cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
+        cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
+        xdata = event.xdata # get event x location
+        ydata = event.ydata # get event y location
+        if event.button == 'up':
+            # deal with zoom in
+            scale_factor = 1/base_scale
+        elif event.button == 'down':
+            # deal with zoom out
+            scale_factor = base_scale
+        else:
+            # deal with something that should never happen
+            scale_factor = 1
+            print(event.button)
+        # set new limits
+        ax.set_xlim([xdata - cur_xrange*scale_factor,
+                     xdata + cur_xrange*scale_factor])
+        ax.set_ylim([ydata - cur_yrange*scale_factor,
+                     ydata + cur_yrange*scale_factor])
+        plt.draw() # force re-draw
+
+    fig = ax.get_figure() # get the figure of interest
+    # attach the call back
+    fig.canvas.mpl_connect('scroll_event',zoom_fun)
+
+    #return the function
+    return zoom_fun
+
+
 class Window(QtWidgets.QWidget):
 
     def __init__(self):
@@ -55,10 +89,10 @@ class Window(QtWidgets.QWidget):
         self.listWidget.addItem(unit_1)
         self.listWidget.addItem(unit_2)
         self.listWidget.addItem(unit_3)
-        self.listWidget.setFixedSize(75,100)
+        self.listWidget.setFixedSize(75, 200)
 
         self.listWidget2 = QtWidgets.QListWidget()
-        self.listWidget2.setFixedSize(75, 100)
+        self.listWidget2.setFixedSize(75, 200)
 
         h_box = QtWidgets.QHBoxLayout()
         h_box.addStretch()
@@ -103,7 +137,8 @@ class Window(QtWidgets.QWidget):
                 plt.yscale('linear')
                 plt.xscale('linear')
                 plt.show()
-
+                scale = 1.5
+                zoom_factory(plt,base_scale=scale)
             except:
                 self.l.setText("Please try to contact your developer...")
 
